ac3ba1955602243b394d3bd35fa2bc5ff9145ba1
Barry Morwood <bmorwood@gmail.com>
2023-04-19 12:34:47 -0400

    updated workflow to remove testing for now

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.(ts|js)x$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j)s$',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node', 'tsx'],
 };
