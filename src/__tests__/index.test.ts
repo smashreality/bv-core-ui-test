7eeeaec5ba6cf694e05059a5c2c5ccc8276678c6
Barry Morwood <bmorwood@gmail.com>
2023-04-18 20:55:53 -0400

    fixed unit test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,13 +1,7 @@
-import uuid from '../index';
+import { echo } from '../index';
 
-describe('valid UUID', () => {
-  let VALID_UUID_REGEX: RegExp;
-
-  beforeAll(() => {
-    VALID_UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-  });
-
-  test('should match a valid UUID', () => {
-    expect(VALID_UUID_REGEX.test(uuid.v4())).toBeTruthy();
+describe('echo', () => {
+  test('should echo', () => {
+    expect(echo('hello')).toBe('echo: hello');
   });
 });
